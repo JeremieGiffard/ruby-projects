a5cc555b47d32da416f37105e26a5f82ddade3f1
oldest-house <8711581-e132784f@users.noreply.gitlab.com>
2025-07-20 15:55:37 +0200

    Caesar_Cipher gestion wrap et uppercase alphabet

--- Caesar_Cipher/Caesar_Cipher.rb
+++ Caesar_Cipher/Caesar_Cipher.rb
@@ -1,25 +1,37 @@
 def caesar_cipher(string_to_encrypt, shift_number = 0)
-  
   # CREER UN HASH AVEC TOUTES LES LETTRES DE L'ALPHABET => NUMBRE INDEX
-  alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  stringToReturn = ''
-  # BOUCLER, POUR CHAQUE LETTRE 
-  string_to_encrypt.each_char { |ch| 
-    # TROUVER L'INDEX CORRESPONDANT DANS LE HASH.
-    temp = alphabet.find_index(ch)
+  alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  stringToReturn = ""
+
+  string_to_encrypt.each_char { |ch|
+    # TROUVER L'INDEX CORRESPONDANT DANS LE HASH
+    index = alphabet.find_index(ch.downcase)
     # checker si ce n'est pas une lettre
-    if temp == nil
+    if index == nil
       stringToReturn << ch
     else
-      # SHIFTER PAR shift_number GERER FIN DE INDEX POUR RETOUR AU DEBUT
-      temp = temp + shift_number
-      stringToReturn << alphabet[temp]
+      index = shift_alphabet(index, shift_number)
+      letter = alphabet[index]
+      if ch == ch.upcase
+        letter = letter.upcase
+      end
+      stringToReturn << letter
     end
-    p temp
   }
   puts stringToReturn
-  # 
-  # RETURN
+  return stringToReturn
 end
 
-caesar_cipher('bonjour !',1)
+# shift le parametre par la valeur du deusième param. wrap from z to a.
+# @param index de la lettre dans l'alphabet
+# @param shift_number valeur à ajouter à l'index
+def shift_alphabet(index, shift_number)
+  lenght_Alphabet = 25
+  shiftTo = index + shift_number
+  if shiftTo - lenght_Alphabet > 0
+    shiftTo = shiftTo - lenght_Alphabet - 1
+  end
+  return shiftTo
+end
+
+caesar_cipher("What a string!", 5)
