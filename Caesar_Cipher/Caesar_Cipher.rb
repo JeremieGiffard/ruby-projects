f1168b66b88205daa2d1e579029f01fe25234da7
oldest-house <8711581-e132784f@users.noreply.gitlab.com>
2025-07-20 14:57:22 +0200

    Caesar_Cipher 1

--- Caesar_Cipher/Caesar_Cipher.rb
+++ Caesar_Cipher/Caesar_Cipher.rb
@@ -1,13 +1,25 @@
 def caesar_cipher(string_to_encrypt, shift_number = 0)
-
-  alphabet = ['a','b','c','d','e']
+  
   # CREER UN HASH AVEC TOUTES LES LETTRES DE L'ALPHABET => NUMBRE INDEX
+  alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  stringToReturn = ''
+  # BOUCLER, POUR CHAQUE LETTRE 
+  string_to_encrypt.each_char { |ch| 
+    # TROUVER L'INDEX CORRESPONDANT DANS LE HASH.
+    temp = alphabet.find_index(ch)
+    # checker si ce n'est pas une lettre
+    if temp == nil
+      stringToReturn << ch
+    else
+      # SHIFTER PAR shift_number GERER FIN DE INDEX POUR RETOUR AU DEBUT
+      temp = temp + shift_number
+      stringToReturn << alphabet[temp]
+    end
+    p temp
+  }
+  puts stringToReturn
   # 
-  # CONVERT string_to_encrypt EN HASH
-  # BOUCLER, POUR CHAQUE LETTRE ET TROUVER L'INDEX CORRESPONDANT DANS LE HASH.
-  # 
-  # SHIFTER PAR shift_number GERER FIN DE INDEX POUR RETOUR AU DEBUT
-  # 
-  # TRANSFORM HASH TO STRING
   # RETURN
 end
+
+caesar_cipher('bonjour !',1)
