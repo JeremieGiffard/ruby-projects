51738646c5ebcfe3def5ae788a3722789f2a784e
oldest-house <8711581-e132784f@users.noreply.gitlab.com>
2025-07-20 13:54:54 +0200

    init subProject caesar_cipher

---
+++ Caesar_Cipher/Caesar_Cipher.rb
@@ -0,0 +1,13 @@
+def caesar_cipher(string_to_encrypt, shift_number = 0)
+
+  alphabet = ['a','b','c','d','e']
+  # CREER UN HASH AVEC TOUTES LES LETTRES DE L'ALPHABET => NUMBRE INDEX
+  # 
+  # CONVERT string_to_encrypt EN HASH
+  # BOUCLER, POUR CHAQUE LETTRE ET TROUVER L'INDEX CORRESPONDANT DANS LE HASH.
+  # 
+  # SHIFTER PAR shift_number GERER FIN DE INDEX POUR RETOUR AU DEBUT
+  # 
+  # TRANSFORM HASH TO STRING
+  # RETURN
+end
