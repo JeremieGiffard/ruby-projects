035ed783a63567626cf55acf47c370358b483ff4
JeremieGiffard <91679287+JeremieGiffard@users.noreply.github.com>
2025-08-03 18:51:59 +0200

    Bubble Sort

--- Bubble_Sort /Bubble_Sort.rb
+++ Bubble_Sort /Bubble_Sort.rb
@@ -1,2 +1,14 @@
-def Bubble_Sort
+def Bubble_Sort(random_array)
+  initial_value = random_array[0]
+  initial_index = 0
+  # loop array
+  random_array.each_with_index do |value, index|
+    if initial_value < value
+      random_array[initial_index] = value
+      random_array[index] = initial_value
+      # do something
+    end
+    # assigner pour comparer prochain loop
+    initial_value = value
+  end
 end
