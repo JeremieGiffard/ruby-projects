4cfaafefde42127e83fcd5b6ca64da3de833a266
JeremieGiffard <91679287+JeremieGiffard@users.noreply.github.com>
2025-09-28 17:34:36 +0200

    ceasar cipher rubocop a

--- Caesar_Cipher/cesar_cipher.rb
+++ Caesar_Cipher/cesar_cipher.rb
@@ -1,25 +1,24 @@
 def caesar_cipher(string_to_encrypt, shift_number = 0)
   # CREER UN HASH AVEC TOUTES LES LETTRES DE L'ALPHABET => NUMBRE INDEX
-  alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  stringToReturn = ""
+  alphabet = %w[a b c d e f g h i j k l m n o p q r s t u
+                v w x y z]
+  stringToReturn = ''
 
-  string_to_encrypt.each_char { |ch|
+  string_to_encrypt.each_char do |ch|
     # TROUVER L'INDEX CORRESPONDANT DANS LE HASH
     index = alphabet.find_index(ch.downcase)
     # checker si ce n'est pas une lettre
-    if index == nil
+    if index.nil?
       stringToReturn << ch
     else
       index = shift_alphabet(index, shift_number)
       letter = alphabet[index]
-      if ch == ch.upcase
-        letter = letter.upcase
-      end
+      letter = letter.upcase if ch == ch.upcase
       stringToReturn << letter
     end
-  }
+  end
   puts stringToReturn
-  return stringToReturn
+  stringToReturn
 end
 
 # shift le parametre par la valeur du deusième param. wrap from z to a.
@@ -28,10 +27,8 @@ end
 def shift_alphabet(index, shift_number)
   lenght_Alphabet = 25
   shiftTo = index + shift_number
-  if shiftTo - lenght_Alphabet > 0
-    shiftTo = shiftTo - lenght_Alphabet - 1
-  end
-  return shiftTo
+  shiftTo = shiftTo - lenght_Alphabet - 1 if shiftTo - lenght_Alphabet > 0
+  shiftTo
 end
 
-caesar_cipher("What a string!", 5)
+caesar_cipher('What a string!', 5)
