7b5ca622253fb45174e4a9cfb7ce609024640476
jeremie <91679287+JeremieGiffard@users.noreply.github.com>
2025-09-28 16:16:08 +0000

    Caesar cipher (#6)
    * caesar cipher
    
    * ceasar cipher rubocop a
    
    * ceasar cipher rubocop a

--- Caesar_Cipher/cesar_cipher.rb
+++ Caesar_Cipher/cesar_cipher.rb
@@ -1,37 +1,33 @@
 def caesar_cipher(string_to_encrypt, shift_number = 0)
   # CREER UN HASH AVEC TOUTES LES LETTRES DE L'ALPHABET => NUMBRE INDEX
-  alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  stringToReturn = ""
+  alphabet = %w[a b c d e f g h i j k l m n o p q r s t u
+                v w x y z]
+  string_to_return = ""
 
-  string_to_encrypt.each_char { |ch|
-    # TROUVER L'INDEX CORRESPONDANT DANS LE HASH
+  string_to_encrypt.each_char do |ch|
     index = alphabet.find_index(ch.downcase)
     # checker si ce n'est pas une lettre
-    if index == nil
-      stringToReturn << ch
+    if index.nil?
+      string_to_return << ch
     else
       index = shift_alphabet(index, shift_number)
       letter = alphabet[index]
-      if ch == ch.upcase
-        letter = letter.upcase
-      end
-      stringToReturn << letter
+      letter = letter.upcase if ch == ch.upcase
+      string_to_return << letter
     end
-  }
-  puts stringToReturn
-  return stringToReturn
+  end
+  puts string_to_return
+  string_to_return
 end
 
 # shift le parametre par la valeur du deusième param. wrap from z to a.
 # @param index de la lettre dans l'alphabet
 # @param shift_number valeur à ajouter à l'index
 def shift_alphabet(index, shift_number)
-  lenght_Alphabet = 25
-  shiftTo = index + shift_number
-  if shiftTo - lenght_Alphabet > 0
-    shiftTo = shiftTo - lenght_Alphabet - 1
-  end
-  return shiftTo
+  lenght_alphabet = 25
+  shift_to = index + shift_number
+  shift_to = shift_to - lenght_alphabet - 1 if (shift_to - lenght_alphabet).positive?
+  shift_to
 end
 
 caesar_cipher("What a string!", 5)
